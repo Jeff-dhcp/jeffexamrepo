27682d42e8c36da68e72b62095d2a889fcb3ac80
Jeff-dhcp <jeff.sidibe888@outlook.com>
2024-10-02 19:07:24 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,26 @@
+variable "subscription_id"{
+  type=string
+}
 
+variable "client_id"{
+  type=string
+}
+variable "client_secret"{
+  type=string
+}
+variable "tenant_id"{
+  type=string
+}
+variable "azurermresourcegroup"{ 
+type=string
+default="myresourcegroup"
+
+}
+variable "loadbalancer" {
+type=string
+default="loadbalancer"
+}
+variable "OSPF" {
+type=string
+default="OSPF"
+}
