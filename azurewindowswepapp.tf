5cc98f49b2c4bd83599ace42aa95e5b1b58392d7
Jeff-dhcp <jeff.sidibe888@outlook.com>
2024-10-02 19:40:38 -0400

    Update and rename azurewindowswepapp to azurewindowswepapp.tf

---
+++ azurewindowswepapp.tf
@@ -0,0 +1,23 @@
+
+
+resource "azurerm_resource_group" "terraformexam" {
+  name     = var.name
+  location = var.location
+}
+
+resource "azurerm_service_plan" "serviceplan" {
+  name                = var.serivce_plan_name
+  resource_group_name = azurerm_resource_group.appterraform.name
+  location            = azurerm_resource_group.westeurope.location
+  sku_name            = var.sku_name
+  os_type             = var.0s_type
+}
+
+resource "azurerm_windows_web_app" "examapp" {
+  name                = "windowsapp"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_service_plan.example.location
+  service_plan_id     = azurerm_service_plan.example.id
+
+  site_config {}
+}
